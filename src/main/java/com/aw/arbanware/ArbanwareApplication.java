f22c0e559a5b5f6520c2b44af7c358b3ab066476
anhy111 <gkdyd123@gmail.com>
2023-09-14 17:14:44 +0900

    first commit

---
+++ src/main/java/com/aw/arbanware/ArbanwareApplication.java
@@ -0,0 +1,13 @@
+package com.aw.arbanware;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArbanwareApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArbanwareApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aw/arbanware/ArbanwareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aw.arbanware;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArbanwareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
