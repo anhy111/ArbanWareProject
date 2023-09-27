1c7674083bf6b985f896fdce9291b27c8791da3b
anhy111 <gkdyd123@gmail.com>
2023-09-27 17:49:34 +0900

    ddl-auto : validate로 변경 경고 일부 제거

--- src/main/java/com/aw/arbanware/domain/review/Recommendation.java
+++ src/main/java/com/aw/arbanware/domain/review/Recommendation.java
@@ -1,6 +1,5 @@
 package com.aw.arbanware.domain.review;
 
-import com.aw.arbanware.domain.common.baseentity.BaseTimeEntity;
 import com.aw.arbanware.domain.user.Member;
 import lombok.Getter;
 import lombok.Setter;
