5514fc379bcda63cc9f0b4384523f8e9bd6faa3e
HISeungYeon <sy.lee5081@gmail.com>
2023-10-03 18:41:55 +0900

    공지사항 뷰 설정

--- src/main/java/com/aw/arbanware/domain/notice/controller/NoticeController.java
+++ src/main/java/com/aw/arbanware/domain/notice/controller/NoticeController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
@@ -21,18 +19,29 @@ public class NoticeController {
 
     private final NoticeService noticeService;
 
-    @GetMapping("/list")
+    @GetMapping("/")
     public String noticeList(Model model) {
         List<Notice> noticeList = noticeService.noticeList();
         model.addAttribute("noticeList", noticeList);
-        return "notice/list";
+        return "page/notice/list";
     }
 
-    @GetMapping("/detail/{id}")
+    @GetMapping("/{id}")
     public String noticeDetail(@PathVariable("id")Long id, Model model){
         //이 뜻은 id값을 통해서 찾았는데 만약 해당 id값이 없다면 null을 반환해라 라는의미입니다.
         Notice noticeDetail = noticeService.noticeDetail(id).orElse(null);
         model.addAttribute("noticeDetail", noticeDetail);
-        return "notice/detail";
+        return "page/notice/detail";
+    }
+
+    @GetMapping("/new")
+    public String noticeRegisterG(){
+        return "page/notice/register";
+    }
+
+    @PostMapping("/new")
+    public String noticeRegister(@ModelAttribute("notice") Notice notice){
+        noticeService.noticeRegister(notice);
+        return "page/notice/list";
     }
 }
