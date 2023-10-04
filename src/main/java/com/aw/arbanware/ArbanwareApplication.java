e69fab93cd608d9cde968083cdfd6573ac72eb77
anhy111 <gkdyd123@gmail.com>
2023-10-04 23:40:23 +0900

    로그인 전 페이지를 요청헤더에서 읽어 로그인 후 그 페이지로 이동 스프링 User객체 파라미터로 받을 수 있도록 수정

--- src/main/java/com/aw/arbanware/ArbanwareApplication.java
+++ src/main/java/com/aw/arbanware/ArbanwareApplication.java
@@ -1,10 +1,13 @@
 package com.aw.arbanware;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 
 @EnableJpaAuditing
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 @SpringBootApplication
 public class ArbanwareApplication {
 
