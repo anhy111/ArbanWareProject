63f290436ccf101337e24c1ac574d7e955475994
anhy111 <gkdyd123@gmail.com>
2023-10-07 17:41:30 +0900

    엔티티 수정 및 DDL 수정

--- src/main/java/com/aw/arbanware/global/config/ArbanwareConfig.java
+++ src/main/java/com/aw/arbanware/global/config/ArbanwareConfig.java
@@ -1,8 +1,13 @@
 package com.aw.arbanware.global.config;
 
+import com.aw.arbanware.global.config.security.SecurityUser;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
 import java.util.UUID;
@@ -12,12 +17,16 @@ public class ArbanwareConfig {
 
     @Bean
     public AuditorAware<String> auditorProvider() {
-        // 지금은 UUID지만 추후에 로그인기능이 구현된 후에
         // 로그인한 사람의 id가 리턴되도록 변경하기
         return new AuditorAware<String>() {
             @Override
             public Optional<String> getCurrentAuditor() {
-                return Optional.of(UUID.randomUUID().toString());
+                final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+                if (authentication instanceof AnonymousAuthenticationToken) {
+                    return Optional.empty();
+                }
+                SecurityUser user = (SecurityUser)authentication.getPrincipal();
+                return Optional.of(user.getId().toString());
             }
         };
     }
