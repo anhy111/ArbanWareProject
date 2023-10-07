f505cc8277b37f59dd3129fd95b9b4faff51fdd7
anhy111 <gkdyd123@gmail.com>
2023-10-07 21:08:12 +0900

    시큐리티 설정

--- src/main/java/com/aw/arbanware/global/config/security/SpringSecurityConfig.java
+++ src/main/java/com/aw/arbanware/global/config/security/SpringSecurityConfig.java
@@ -3,7 +3,9 @@ package com.aw.arbanware.global.config.security;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,6 +17,8 @@ import javax.servlet.DispatcherType;
 import javax.servlet.http.HttpServletRequest;
 
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class SpringSecurityConfig {
 
     @Bean
