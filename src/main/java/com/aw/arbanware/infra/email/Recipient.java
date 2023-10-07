a89132b38f1bc833c9d8dcacf00705a396f3382d
anhy111 <gkdyd123@gmail.com>
2023-10-08 04:47:29 +0900

    #21 Rebase onto master

--- src/main/java/com/aw/arbanware/infra/email/Recipient.java
+++ src/main/java/com/aw/arbanware/infra/email/Recipient.java
@@ -3,10 +3,8 @@ package com.aw.arbanware.infra.email;
 
 import lombok.Getter;
 import lombok.Setter;
-import lombok.ToString;
 
 @Getter @Setter
-@ToString(of = {"address","name"})
 public class Recipient {
     private String address;
     private String name;
