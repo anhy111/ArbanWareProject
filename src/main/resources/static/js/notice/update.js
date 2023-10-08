5d7017173e99f54c3c243b9183a1ef89e29215ca
HISeungYeon <sy.lee5081@gmail.com>
2023-10-09 01:17:30 +0900

    공지사항 수정

--- src/main/resources/static/js/notice/update.js
+++ src/main/resources/static/js/notice/update.js
@@ -1,37 +1,38 @@
 $(function(){
     // alert("하이");
 
-    let title = $('#title').val();
-    let content = $('#content').val();
+    let titleUpdate = $('#title').val();
+    let contentUpdate = $('#content').val();
 
     let today = new Date();
 
+    $('#title').val(title);
+    $('#name').val(name);
+    $('#content').val(content);
     $('#today').text(today.toLocaleDateString());
-
 
     $('#cancel').click(function (){
         $(location).attr('href', '/notice')
     });
 
-    $('#register').click(function (){
-
-        let data = {'title':title, 'content':content}
+    $('#update').click(function (){
+        let data = {'title':titleUpdate, 'content':contentUpdate}
 
         Swal.fire({
-            title: '등록 하시겠습니까?',
+            title: '수정 하시겠습니까?',
             // text: "한번 등록한 내용은 되돌릴 수 없습니다.",
             icon: 'question',
             showCancelButton: true,
             confirmButtonColor: '#3085d6',
             cancelButtonColor: '#d33',
-            confirmButtonText: '등록',
+            confirmButtonText: '수정',
             cancelButtonText: '취소'
         }).then(function(result) {
             console.log(">>>>>>>>>>>result", result);
             if(result.value){
                 $.ajax({
                     type: 'post',
-                    url: '/notice/new',
+                    url: '/notice/'+id+'/edit',
                     contentType:"application/json;charset=utf-8",
                     data:JSON.stringify(data),
                     // beforeSend:function(xhr){
@@ -41,7 +42,7 @@ $(function(){
                         console.log("delete성공이라해주라 ", data);
 
                         Swal.fire(
-                            '등록 완료',
+                            '수정 완료',
                             '정상적으로 등록 되었습니다.',
                             'success'
                         ).then(function(){
@@ -52,7 +53,7 @@ $(function(){
                     error:function(request, status, error){
                         console.log("code : "+request.status+"\n"+"message : "+request.responseText+"\n"+"error : "+error);
                         Swal.fire(
-                            "등록 실패",
+                            "수정 실패",
                             "에러 났어요!", // had a missing comma
                             "error"
                         )
