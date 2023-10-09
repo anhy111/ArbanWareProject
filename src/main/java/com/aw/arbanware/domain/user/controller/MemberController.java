e0555c96b61eb5e8f72572ff93a45e919b097ae2
anhy111 <gkdyd123@gmail.com>
2023-10-10 01:19:04 +0900

    이메일 인증 완료

--- src/main/java/com/aw/arbanware/domain/user/controller/MemberController.java
+++ src/main/java/com/aw/arbanware/domain/user/controller/MemberController.java
@@ -1,14 +1,24 @@
 package com.aw.arbanware.domain.user.controller;
 
+import com.aw.arbanware.domain.common.apiobj.DefaultResponse;
+import com.aw.arbanware.domain.common.apiobj.ResponseMessage;
+import com.aw.arbanware.domain.common.apiobj.StatusCode;
 import com.aw.arbanware.domain.user.service.MemberService;
 import com.aw.arbanware.infra.email.Email;
 import com.aw.arbanware.infra.email.EmailService;
+import com.aw.arbanware.infra.email.EmailStatus;
 import com.aw.arbanware.infra.email.Recipient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.util.Map;
 
 @Controller
 @RequiredArgsConstructor
@@ -30,9 +40,43 @@ public class MemberController {
 
     @PostMapping("/members/email")
     @ResponseBody
-    public String email(@RequestBody Recipient recipient){
-        EmailService.sendMail(new Email(recipient));
+    public String email(@RequestBody Recipient recipient, HttpSession session){
+        final String randomNum = createRandomNum();
+        final EmailStatus emailStatus = EmailService.sendMail(new Email(recipient), randomNum);
         log.info("recipient = {}", recipient);
+        if (emailStatus == EmailStatus.FAIL) {
+            return "fail";
+        }
+        session.setAttribute("emailAuth", randomNum);
+
         return "success";
+    }
+
+    @PostMapping("/members/emailCheck")
+    @ResponseBody
+    public ResponseEntity emailCheck(@RequestParam("emailAuthInput") String emailAuthInput
+                                                                , HttpSession session) {
+        final Object obj = session.getAttribute("emailAuth");
+        if (obj == null) {
+            return new ResponseEntity(new DefaultResponse(StatusCode.BAD_REQUEST, ResponseMessage.NOT_REQ_EMAIL)
+                    , HttpStatus.OK);
+        }
+
+        final String emailAuth = (String) obj;
+        log.info("session auth={}", emailAuth);
+        log.info("user.auth={}", emailAuthInput);
+
+        if (!emailAuth.equals(emailAuthInput)) {
+            return new ResponseEntity(new DefaultResponse(StatusCode.BAD_REQUEST, ResponseMessage.AUTH_FAIL)
+                    , HttpStatus.OK);
+        }
+        return new ResponseEntity(new DefaultResponse(StatusCode.OK, ResponseMessage.EMAIL_AUTH_SUCCESS)
+                , HttpStatus.OK);
+    }
+
+    private static String createRandomNum() {
+        java.util.Random generator = new java.util.Random();
+        generator.setSeed(System.currentTimeMillis());
+        return String.valueOf(generator.nextInt(1000000) % 1000000);
     }
 }
