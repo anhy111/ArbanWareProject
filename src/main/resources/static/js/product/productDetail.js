ec1a07b6e078579a47cb9af2b0ac9324fd035959
anhy111 <gkdyd123@gmail.com>
2023-10-29 23:38:18 +0900

    상품상세 ui조정

--- src/main/resources/static/js/product/productDetail.js
+++ src/main/resources/static/js/product/productDetail.js
@@ -1,4 +1,7 @@
 $(document).ready(function () {
+
+    let $amount = $('#amount');
+
     ClassicEditor
         .create( document.querySelector( '#content' ) )
         .then( editor => {
@@ -14,4 +17,39 @@ $(document).ready(function () {
             } );
             editor.enableReadOnlyMode('#content');
         } );
+
+    $("#subAmount").on('click', function () {
+        let val = $amount.val();
+
+        if (!regNumberAndSwal(val)) {
+            return;
+        }
+
+        if (val <= 1) {
+            return;
+        }
+
+        $amount.val($amount.val() - 1);
+    });
+
+    $("#addAmount").on('click', function () {
+        let val = $amount.val();
+        if (!regNumberAndSwal(val)) {
+            return;
+        }
+
+        $amount.val(val - 0 + 1);
+    });
+
+    function regNumberAndSwal(val) {
+        let regExp = /^[0-9]*$/g;
+        if (regExp.test(val)) {
+            return true;
+        }
+        Swal.fire({
+            html:'<b>숫자만 입력해주세요.</b>',
+            icon : 'error'
+        })
+        return false;
+    }
 });
