c050db279464b077d85e0a31077759a5791aa483
anhy111 <gkdyd123@gmail.com>
2023-11-28 04:43:18 +0900

    신상품 조회

--- src/main/java/com/aw/arbanware/domain/MainController.java
+++ src/main/java/com/aw/arbanware/domain/MainController.java
@@ -1,13 +1,33 @@
 package com.aw.arbanware.domain;
 
+import com.aw.arbanware.domain.product.controller.ProductSearchCondition;
+import com.aw.arbanware.domain.product.repository.ProductProductInfoDto;
+import com.aw.arbanware.domain.product.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class MainController {
 
+    private final ProductService productService;
+
     @GetMapping("/")
-    public String main(){
+    public String main(Model model){
+
+        final PageRequest newProductRequest = PageRequest.of(0, 4, Sort.Direction.DESC, "registrationTime");
+        final Page<ProductProductInfoDto> newProducts = productService.searchProducts(new ProductSearchCondition(), newProductRequest);
+
+        final PageRequest bestProductRequest = PageRequest.of(0, 4, Sort.Direction.DESC, "buyCount");
+        final Page<ProductProductInfoDto> bestProducts = productService.searchProducts(new ProductSearchCondition(), bestProductRequest);
+
+        model.addAttribute("newProducts", newProducts);
+        model.addAttribute("bestProducts", bestProducts);
         return "page/index";
     }
 }
