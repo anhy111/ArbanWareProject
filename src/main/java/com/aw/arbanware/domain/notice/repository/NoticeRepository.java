e42859fa1a5f6c18862465889fa76fbc6ee4b0ac
HISeungYeon <sy.lee5081@gmail.com>
2023-12-08 02:39:30 +0900

    뷰 수정

--- src/main/java/com/aw/arbanware/domain/notice/repository/NoticeRepository.java
+++ src/main/java/com/aw/arbanware/domain/notice/repository/NoticeRepository.java
@@ -1,7 +1,6 @@
 package com.aw.arbanware.domain.notice.repository;
 
 import com.aw.arbanware.domain.notice.entity.Notice;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
