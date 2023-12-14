c3ef395a1fbebdd895cdeda86cb949ae903b2e37
anhy111 <gkdyd123@gmail.com>
2023-12-14 19:59:18 +0900

    배포시 h2 자동 켜짐

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,17 @@ JAR_NAME=$(ls $REPOSITORY/build/libs/ | grep 'SNAPSHOT.jar' | tail -n 1)
 JAR_PATH=$REPOSITORY/build/libs/$JAR_NAME
 
 CURRENT_PID=$(pgrep -f $APP_NAME)
+CURRENT_PID_H2=$(pgrep -f h2)
+
+if [ -z $CURRENT_PID_H2 ]
+then
+  echo "> 종료할 h2서버 없음."
+else
+  echo "> kill -9 $CURRENT_PID_H2"
+  kill -15 $CURRENT_PID_H2
+  sleep 5
+fi
+nohup java -cp /home/ec2-user/h2/bin/h2*.jar org.h2.tools.Server -tcp &
 
 if [ -z $CURRENT_PID ]
 then
