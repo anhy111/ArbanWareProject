b43bdfaecf44498f824542f086691c9d1c736c0c
anhy111 <gkdyd123@gmail.com>
2024-09-12 02:34:27 +0900

    로그파일 권한 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -33,8 +33,10 @@ echo "> $JAR_PATH 배포"
 
 touch $REPOSITORY/arbanware.log
 cat /dev/null> $REPOSITORY/arbanware.log
+chown ec2-user:ec2-user $REPOSITORY/arbanware.log
 
 touch $REPOSITORY/arbanware_error.log
 cat /dev/null> $REPOSITORY/arbanware_error.log
+chown ec2-user:ec2-user $REPOSITORY/arbanware_error.log
 
 nohup java -jar $JAR_PATH 1> $REPOSITORY/arbanware.log 2>$REPOSITORY/arbanware_error.log &
