3f438975c38990b1b33560ebbe43cae980b59e92
anhy111 <gkdyd123@gmail.com>
2024-09-12 04:57:24 +0900

    mysql 마이그레이션 변경사항 hibername 버전5부터 기본 IDENTITY전략은 table이다. auto_increament적용을 위해 id-mapping을 꺼주면 TABLE전략을 사용하지 않는다.

--- src/main/java/com/aw/arbanware/domain/order/repository/OrderRepository.java
+++ src/main/java/com/aw/arbanware/domain/order/repository/OrderRepository.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order, String> {
 
-    @Query(value = "SELECT NEXT VALUE FOR ORDERS_SEQUENCE", nativeQuery = true)
+    @Query(value = "SELECT nextVal(\"ORDERS_SEQUENCE\") FROM DUAL", nativeQuery = true)
     Long findSequence();
 
     @Query("select distinct o from Order o left join fetch o.orderProduct op left join fetch op.productInfo pi left join fetch pi.product p where o.member.id = :memberId order by o.orderDate desc")
